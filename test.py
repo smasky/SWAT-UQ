9dea0609984bba6f9fd9cd49a75aaf4797ba174b
Smasky <492109831@qq.com>
2024-10-14 16:24:12 +0800

    'UI_scaler'

--- test.py
+++ test.py
@@ -1,19 +1,36 @@
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QDesktopWidget
 from PyQt5.QtCore import Qt
 import sys
 from GUI import MainWindow
 import os
 
+def calculate_scale_factor(target_width, target_height, base_width=3072, base_height=1920, base_scale=1.8):
+    # 计算宽度和高度比例
+    width_ratio = target_width / base_width
+    height_ratio = target_height / base_height
+    
+    # 选择较小的比例以保持纵横比
+    scale_ratio = min(width_ratio, height_ratio)
+    
+    # 计算新的缩放系数
+    new_scale = base_scale * scale_ratio
+    return new_scale
+
 
 if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    screen = QDesktopWidget().screenGeometry()
+    width = screen.width()
+    height = screen.height()
     
+    scaler=calculate_scale_factor(width, height)
     os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"  # 禁用 Qt 自动缩放
-    os.environ["QT_SCALE_FACTOR"] = "1.8"  # 禁用 Qt 自动缩放
+    os.environ["QT_SCALE_FACTOR"] = f"{scaler}"  # 禁用 Qt 自动缩放
     
     QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)  # 使能高 DPI 缩放
     QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)  # 使得图片元素也遵循缩放
-    app = QApplication(sys.argv)
+    
     demo = MainWindow()
     demo.show()
     app.exec()
