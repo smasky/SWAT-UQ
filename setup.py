2303bd2a69b5e6ac76d592b3f5c5a9abb4a7aa91
Smasky <492109831@qq.com>
2025-04-09 15:36:40 +0800

    '0409'

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, Extension, find_packages
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+import numpy
+import sys
+import pybind11
+from pathlib import Path
+
+numpy_inc = numpy.get_include()
+pybind11_inc = pybind11.get_include()
+
+pybind11_extensions = [
+    Extension("swatuq.utility", [str(Path("swatuq/swat_utility.cpp"))], extra_compile_args=['-std=c++17'] if sys.platform != 'win32' else ['/std:c++20'], include_dirs=[numpy_inc, pybind11_inc]),
+]
+
+setup(
+    name="swatuq",
+    version="1.0.0",
+    author="wmtSky",
+    author_email="wmtsmasky@gmail.com",
+    description="A Python module that extends Python with C++ code using Pybind11.",
+    ext_modules=pybind11_extensions,
+    classifiers=[
+        # 添加适合的类目，例如
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
---
+++ swatuq/__init__.py
@@ -0,0 +1,5 @@
+from .swat_uq import SWAT_UQ
+
+__version__ = "1.0.0"   
+__author__ = "wmtSky"
+
