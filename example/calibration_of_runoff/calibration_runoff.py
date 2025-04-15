0a532f26f642092c0573d035bbcaf9d7796a82d7
Smasky <492109831@qq.com>
2025-04-15 22:21:34 +0800

    '0415'

--- example/calibration_of_runoff/calibration_runoff.py
+++ example/calibration_of_runoff/calibration_runoff.py
@@ -3,10 +3,11 @@ sys.path.insert(0, '.')
 
 from swatuq import SWAT_UQ
 
-projectPath = "D:\\example_runoff\\TxtInOutFSB"
+
+projectPath = "E:\\example_runoff\\TxtInOutFSB"
 exeName = "swat.exe"
 
-workPath = "D:\\example_runoff"
+workPath = "E:\\example_runoff"
 parFile = "scenario1.par"
 evalFile = "scenario1.obj"
 
@@ -14,8 +15,14 @@ problem = SWAT_UQ(projectPath = projectPath, swatExeName = exeName,
                     workPath = workPath, paraFileName = parFile, evalFileName = evalFile,
                     verboseFlag = True, numParallel = 10, optType = 'max')
 
-from UQPyL.optimization import GA
+from UQPyL.sensibility import FAST
 
-ga = GA(nPop = 50, verboseFreq = 1)
+fast = FAST()
 
-ga.run(problem = problem)
+X = fast.sample(problem, 100)
+
+Y = problem.evaluate(X)
+
+res =fast.analyze(problem, X, Y)
+
+print(res)
