5fc76418dd7936a394fd81d414c968d0b1ac7dc1
Smasky <492109831@qq.com>
2025-04-25 15:32:23 +0800

    '0425'

--- example/example1/sensitivity_analysis.py
+++ example/example1/sensitivity_analysis.py
@@ -1,25 +1,21 @@
-import sys
-sys.path.insert(0, '.')
-
 from swatuq import SWAT_UQ
 
-
-projectPath = "E:\\example_runoff\\TxtInOutFSB"
+projectPath = "E:\\projectPath"
 exeName = "swat.exe"
 
-workPath = "E:\\example_runoff"
-parFile = "scenario1.par"
-evalFile = "scenario1.obj"
+workPath = "E:\\workPath"
+parFile = "para_sa.par"
+evalFile = "obj_sa.evl"
 
 problem = SWAT_UQ(projectPath = projectPath, swatExeName = exeName,
                     workPath = workPath, paraFileName = parFile, evalFileName = evalFile,
-                    verboseFlag = True, numParallel = 10, optType = 'max')
+                    verboseFlag = True, numParallel = 20, optType = 'max')
 
 from UQPyL.sensibility import FAST
 
-fast = FAST()
+fast = FAST(saveFlag = True)
 
-X = fast.sample(problem, 100)
+X = fast.sample(problem, 256)
 
 Y = problem.evaluate(X)
 
