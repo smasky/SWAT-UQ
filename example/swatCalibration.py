b69aa22c9dc2c870e683ebb9eac7e330c60851cf
Smasky <492109831@qq.com>
2025-04-26 11:17:57 +0800

    '0426'

--- example/swatCalibration.py
+++ example/swatCalibration.py
@@ -1,27 +1,31 @@
 import sys
 sys.path.append(".")
 
-
+import numpy as np
 from swatuq import SWAT_UQ
 from UQPyL.optimization.single_objective import GA
 
 nInput = 24
 nOutput = 1
 
-projectPath = "D:\\djBasin\\TxtInOutFSB\\TxtInOutFSB"
+projectPath = "E:\\DJBasin\\TxtInOutFSB"
 exeName = "swat.exe"
-workPath = "D:\\DJ_FSB"
-paraFileName = "para_sa.par"
-evalFileName = "obj_sa.evl"
+workPath = "E:\\DJ_FSB"
+paraFileName = "op.par"
+evalFileName = "op.evl"
 
 problem = SWAT_UQ(projectPath = projectPath, swatExeName = exeName,
                     workPath = workPath, paraFileName = paraFileName, evalFileName = evalFileName,
                     verboseFlag = True, numParallel = 1)
 
-from UQPyL.DoE import LHS
+# from UQPyL.DoE import LHS
 
-lhs = LHS()
+# lhs = LHS()
 
-X = lhs.sample(nt = 100, problem = problem)
+# X = lhs.sample(nt = 100, problem = problem)
 
-problem.objFunc(X[0:1, :])
+X = np.array([-0.0781, 0.3761, 0.6200, 1.1167, 8.1841, 175.5632, 0.8667, 11.9633, 0.9307, 8.6665, 31.4787, 0.6447])
+
+y = problem.objFunc(X.reshape(1, -1))
+
+print(y)
